913b60a17d3343035bab18540b2265afdaf6c4b4
Alikhan <anuspekov@bilgenai.com>
2024-09-23 12:40:59 +0500

    resolved merge conflicts after pulling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,26 @@
-import Aura from '@primevue/themes/aura';
+import Aura from "@primevue/themes/aura";
 
 export default defineNuxtConfig({
-    modules: [
-      '@primevue/nuxt-module'
-  ],
-    primevue: {
-      options: {
-          theme: {
-              preset: Aura
-          }
-      }
+  modules: ["@primevue/nuxt-module"],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
   },
   css: [
-    'bootstrap/dist/css/bootstrap.min.css',
-    'primeicons/primeicons.css',
-    'bootstrap-icons/font/bootstrap-icons.css'
+    "bootstrap/dist/css/bootstrap.min.css",
+    "primeicons/primeicons.css",
+    "bootstrap-icons/font/bootstrap-icons.css",
   ],
   vite: {
     define: {
-      'process.env.DEBUG': false
-    }
+      "process.env.DEBUG": false,
+    },
   },
   build: {
-    transpile: ['bootstrap']
+    transpile: ["bootstrap"],
   },
-  plugins: [
-    { src: '~/plugins/bootstrap.client.ts', mode: 'client' }
-  ]
-})
+  plugins: [{ src: "~/plugins/bootstrap.client.ts", mode: "client" }],
+});
