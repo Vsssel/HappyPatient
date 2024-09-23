b4f80220cc5ea9bd99d0ff5a4c5b43606864fed4
Alikhan <anuspekov@bilgenai.com>
2024-09-23 14:48:04 +0500

    updated loader

--- shared/Loader.vue
+++ shared/Loader.vue
@@ -2,10 +2,10 @@
   <div>
     <div v-if="isLoading">
       <div v-if="type === 'square'">
-        <Skeleton shape="square" width="100px" height="100px" />
+        <Skeleton :style="sizeStyle" shape="square" />
       </div>
       <div v-else-if="type === 'line'">
-        <Skeleton width="100%" height="2rem" />
+        <Skeleton :style="sizeStyle" />
       </div>
       <div v-else>
         <Skeleton width="6rem" height="6rem" />
@@ -20,29 +20,33 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
 import Skeleton from 'primevue/skeleton';
+import { computed } from 'vue';
 
 const props = defineProps({
-  url: {
-    type: String,
+  isLoading: {
+    type: Boolean,
     required: true,
   },
   type: {
     type: String,
     default: 'default',
   },
+  size: {
+    type: Object,
+    default: () => ({
+      width: '100%',
+      height: 'auto',
+    }),
+  },
+  data: {
+    type: [Object, null],
+    default: null,
+  },
 });
 
-const isLoading = ref(true);
-const data = ref(null);
-
-onMounted(async () => {
-  try {
-    const response = await fetch(props.url);
-    data.value = await response.json();
-  } finally {
-    isLoading.value = false;
-  }
-});
+const sizeStyle = computed(() => ({
+  width: props.size.width || '100%',
+  height: props.size.height || 'auto',
+}));
 </script>
