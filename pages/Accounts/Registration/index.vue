683be944c0e751e3727b8bea2e091d32109cf7a9
Alikhan <anuspekov@bilgenai.com>
2024-10-03 15:24:34 +0500

    rere

--- pages/Accounts/Registration/index.vue
+++ pages/Accounts/Registration/index.vue
@@ -150,7 +150,7 @@ const onSubmit = async (fieldValues: Record<string, any>) => {
 
   try {
     const response = await fetch(
-      'http://172.20.10.8:8000/patient/auth/sign-up',
+      'http://172.20.10.13:8000/patient/auth/sign-up',
       {
         method: 'POST',
         headers: {
